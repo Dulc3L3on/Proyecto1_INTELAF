124b118ce1f05b09ae2e699c345749e3318a5ab2
Dulc3L3on <ldulce2001@gmail.com>
2020-08-08 16:50:31 -0600

    Se ha creado la estructura visual

---
+++ src/main/java/com/mycompany/proyecto1_intelaf/PrincipalTrabajadores.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1_intelaf;
+    
+import FrontendTrabajadores.Consultas;
+import FrontendTrabajadores.Home;
+import FrontendTrabajadores.ModoCajero;
+import FrontendTrabajadores.ModoGerente;
+import FrontendTrabajadores.Reportes;
+
+
+/**
+ *
+ * @author phily
+ */
+public class PrincipalTrabajadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home home = new Home();        
+        
+        home.setLocationRelativeTo(null);
+        home.setVisible(true);
+        
+    }
+    
+}
